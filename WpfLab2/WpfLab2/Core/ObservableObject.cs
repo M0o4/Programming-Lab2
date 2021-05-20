09c4862a5ba78ac5bac4cc741b1b6cacad6268dc
M0o4 <indrei457@gmail.com>
2021-05-20 19:34:28 +0300

    ui

---
+++ WpfLab2/WpfLab2/Core/ObservableObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfLab2.Core
+{
+	class ObservableObject : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void OnPropertyChanged([CallerMemberName] string name = "")
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+		}
+	}
+}
---
+++ WpfLab2/WpfLab2/Core/RelayCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WpfLab2.Core
+{
+	class RelayCommand : ICommand
+	{
+		private Action<object> _execute;
+		private Func<object, bool> _canExecute;
+
+		public event EventHandler CanExecuteChanged
+		{
+			add { CommandManager.RequerySuggested += value; }
+			remove { CommandManager.RequerySuggested -= value; }
+		}
+
+		public bool CanExecute(object parameter) => _canExecute == null || _canExecute(parameter);
+
+		public void Execute(object parameter) => _execute(parameter);
+
+		public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+		{
+			_execute = execute;
+			_canExecute = canExecute;
+		}
+	}
+}
