127acfd117540cd943d10c5e99200bf8f5c2d85e
M0o4 <indrei457@gmail.com>
2021-05-21 19:30:44 +0300

    view model

---
+++ WpfLab2/WpfLab2/MVVM/ViewModel/DiscoveryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfLab2.MVVM.ViewModel
+{
+	class DiscoveryViewModel
+	{
+	}
+}
--- WpfLab2/WpfLab2/MVVM/ViewModel/MainViewModel.cs
+++ WpfLab2/WpfLab2/MVVM/ViewModel/MainViewModel.cs
@@ -5,9 +5,40 @@ namespace WpfLab2.MVVM.ViewModel
 {
 	class MainViewModel : ObservableObject
 	{
+		public RelayCommand HomeViewCommand { get; set; }
+		public RelayCommand DiscoveryViewCommand { get; set; }
+
+		public HomeViewModel HomeVM { get; set; }
+		public DiscoveryViewModel DiscoveryVM { get; set; }
+
+		private object _currentView;
+
+		public object CurrentView
+		{
+			get { return _currentView; }
+			set 
+			{ 
+				_currentView = value;
+				OnPropertyChanged();
+			}
+		}
+
+
 		public MainViewModel()
 		{
+			HomeVM = new HomeViewModel();
+			DiscoveryVM = new DiscoveryViewModel();
+			CurrentView = HomeVM;
 
+			HomeViewCommand = new RelayCommand(o => 
+			{
+				CurrentView = HomeVM;
+			});
+
+			DiscoveryViewCommand = new RelayCommand(o =>
+			{
+				CurrentView = DiscoveryVM;
+			});
 		}
 	}
 }
